70ef7db10989e1aa128e363a39bbca39cd08cbee
Guirdo <produccionesaldo@gmail.com>
2022-12-07 14:04:13 -0600

    Add vite.config.js file for building configuration
    - Set root, outDir and publicDir paths
    - Add multipage configuration for building of the project
    - Set output filename configuration without hash option

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+/* eslint-disable max-len */
+import glob from 'glob';
+import {resolve, relative, extname, basename} from 'path';
+import {defineConfig} from 'vite';
+
+const root = resolve(__dirname, 'pages');
+const outDir = resolve(__dirname, 'dist');
+const publicDir = resolve(__dirname, 'public');
+
+const input = Object.fromEntries(glob.sync('pages/**/*.html').map((file) => {
+  const filePath = resolve(root, relative('pages', file));
+  let fileEntry;
+
+  if (basename(file) !== 'index.html') {
+    fileEntry = basename(file).substring(0, basename(file).length - extname(file).length);
+  } else {
+    fileEntry = relative('pages', file.slice(0, file.length -basename(file).length));
+    // If fileEntry is an empty string, then it's the main page
+    fileEntry = fileEntry === '' ? 'main' : fileEntry;
+  }
+
+  return [fileEntry, filePath];
+}));
+
+export default defineConfig({
+  root,
+  publicDir,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input,
+      output: {
+        // Removing hash from output files
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+});
