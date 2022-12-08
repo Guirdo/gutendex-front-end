b422df21633d0e1f3bfe98b24846139402c616e4
Guirdo <produccionesaldo@gmail.com>
2022-12-08 15:52:34 -0600

    Fetch books
    - Add advancedSearchForm constant
    - Add author, title, topic & language constants
    - Add submit event listener in advancedSearchForm
    - Add prepareApiURL function in order to format the api request url

--- pages/main.js
+++ pages/main.js
@@ -1,2 +1,29 @@
+/* eslint-disable require-jsdoc */
 import '../style/global.scss';
+
+const advancedSearchForm = document.querySelector('#advancedSearchForm');
+
+function prepareApiURL(author, title, topic, language) {
+  return encodeURI(
+    'https://gutendex.com/books?' +
+    ((author || title) && `search=${author && author + ' '}${title}`) +
+    (topic && `&topic=${topic}`) +
+    (language && `&languages=${language}`),
+  );
+}
+
+advancedSearchForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const author = document.querySelector('#author').value;
+  const title = document.querySelector('#title').value;
+  const topic = document.querySelector('#topic').value;
+  const language = document.querySelector('#language').value;
+
+  const url = prepareApiURL(author, title, topic, language);
+
+  const bookList = await fetch(url)
+      .then((res) => res.json())
+      .catch((error) => console.log(error));
+});
 
