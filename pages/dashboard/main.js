6768c4ac8f6dd365838ad2ba70fcfca20022af03
Guirdo <produccionesaldo@gmail.com>
2022-12-13 21:41:16 -0600

    add the dashboard bar with quick information
    - show the total searches made by the user
    - show the total books added by the user

--- pages/dashboard/main.js
+++ pages/dashboard/main.js
@@ -1,2 +1,13 @@
 import '../../style/global.scss';
+import UserBehaviourDataBase from '../../store/userBehaviour';
+import BookDataBase from '../../store/book';
 
+const userBehaviourDB = new UserBehaviourDataBase();
+const bookDB = new BookDataBase();
+const totalSearches = document.querySelector('#totalSearches');
+const totalAddedBooks = document.querySelector('#totalAddedBooks');
+
+// Shows the total searches made by the user
+totalSearches.innerHTML = userBehaviourDB.getTotalSearches();
+// Show the total books added by the user
+totalAddedBooks.innerHTML = bookDB.getTotalBooks();
