995363ef93cacfbc5266e62332163e932405384b
Guirdo <produccionesaldo@gmail.com>
2022-12-18 15:51:39 -0600

    add searchBook function in the BookDataBase class

--- store/book.js
+++ store/book.js
@@ -12,6 +12,31 @@ class BookDataBase {
   getTotalBooks() {
     return this.books.length;
   }
+
+  async searchBook(author, title, topic, language) {
+    const results = await this.books.filter((book) => {
+      const bookAuthor = book.authors[0]['name'].toLowerCase();
+      const bookTitle = book.title.toLowerCase();
+      const bookSubjects = book.subjects[0].toLowerCase();
+      if (bookAuthor.includes(author) && author !== '') {
+        return book.languages[0] === language;
+      }
+      if (bookTitle.includes(title) && title !== '') {
+        return book.languages[0] === language;
+      }
+      if (bookSubjects.includes(topic) && topic !== '') {
+        return book.languages[0] === language;
+      }
+      return false;
+    });
+
+    return {
+      count: results.length,
+      next: null,
+      previous: null,
+      results,
+    };
+  }
 }
 
 export default BookDataBase;
